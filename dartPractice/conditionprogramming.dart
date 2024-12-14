b03ae4485581af9693a3d1988ac7f658b8b8eff5
Muhammad Tayyab <tayyabxtrem@gmail.com>
2024-12-14 15:56:21 +0500

    dart completed

---
+++ dartPractice/conditionprogramming.dart
@@ -0,0 +1,57 @@
+void main() {
+  if (5 > 2) {
+    print('5 is greater than 2');
+  }
+  if (5 < 2) {
+    print('5 is less than 2');
+  } else {
+    print('5 is not less than 2');
+  }
+  if (5 == 2) {
+    print('5 is equal to 2');
+  } else {
+    print('5 is not equal to 2');
+  }
+  if (5 != 2) {
+    print('5 is not equal to 2');
+  } else {
+    print('5 is equal to 2');
+  }
+  if (5 >= 2) {
+    print('5 is greater than or equal to 2');
+  } else {
+    print('5 is not greater than or equal to 2');
+  }
+  if (5 <= 2) {
+    print('5 is less than or equal to 2');
+  } else {
+    print('5 is not less than or equal to 2');
+  }
+  if (5 > 2 && 5 < 10) {
+    print('5 is greater than 2 and less than 10');
+  } else {
+    print('5 is not greater than 2 and less than 10');
+  }
+  if (5 > 2 || 5 > 10) {
+    print('5 is greater than 2 or 5 is greater than 10');
+  } else {
+    print('5 is not greater than 2 or 5 is not greater than 10');
+  }
+  if (!(5 > 2)) {
+    print('5 is not greater than 2');
+  } else {
+    print('5 is greater than 2');
+  }
+  var age = 25;
+  if (age > 18) {
+    print('You are eligible to vote');
+  } else {
+    print('You are not eligible to vote');
+  }
+  var name = 'John';
+  if (name == 'John') {
+    print('Welcome John');
+  } else {
+    print('You are not John');
+  }
+}
